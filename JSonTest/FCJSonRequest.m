53cf44a99a4721d87377de0e5a46d35b29d574fd
user01 <user01@mac-mini-user01.local>
2016-09-14 23:07:46 +0300

    added proj with base VisualObjects

---
+++ JSonTest/FCJSonRequest.m
@@ -0,0 +1,16 @@
+//
+//  FCJSonRequest.m
+//  JSonTest
+//
+//  Created by user01 on 14.09.16.
+//  Copyright © 2016 user01. All rights reserved.
+//
+
+#import "FCJSonRequest.h"
+#import "FCImage.h"
+#import <RestKit/RestKit.h>
+
+
+@implementation FCJSonRequest
+
+@end
---
+++ JSonTest/FCVisualItem.h
@@ -0,0 +1,23 @@
+//
+//  FCVisualItem.h
+//  JSonTest
+//
+//  Created by user01 on 14.09.16.
+//  Copyright © 2016 user01. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <RestKit/RestKit.h>
+
+/**
+ * Base class of viewing object.
+ */
+@interface FCVisualItem : NSObject
+
+@property (strong, nonatomic) NSString* url;
+@property (strong, nonatomic) NSString* identifier;
+@property CGSize size;
+
+@end
+
