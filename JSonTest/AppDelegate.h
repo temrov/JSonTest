1d4bda9cf7c31790f8c0ecadf074535223cb8d0e
user01 <user01@mac-mini-user01.local>
2016-09-14 20:25:22 +0300

    Initial Commit

---
+++ JSonTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSonTest
+//
+//  Created by user01 on 14.09.16.
+//  Copyright © 2016 user01. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
